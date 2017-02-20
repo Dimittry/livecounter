4275815e70f9d499c2560f1287ee9942d8dd07ff
olyver <demar.07@gmail.com>
2017-02-21 01:54:23 +0900

    Has been added executors.

---
+++ saver/src/main/java/com/livecounter/saver/executor/Executor.java
@@ -0,0 +1,48 @@
+package com.livecounter.saver.executor;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Executor {
+    private final Connection connection;
+
+    public Executor(Connection connection) {
+        this.connection = connection;
+    }
+
+    public int execUpdate(final String update) throws SQLException {
+        Statement stmt = connection.createStatement();
+        stmt.execute(update);
+        int updated = stmt.getUpdateCount();
+        stmt.close();
+        connection.close();
+        return updated;
+    }
+
+    public int[] execBatchUpdate(final String[] queries) throws SQLException{
+        connection.setAutoCommit(false);
+        Statement stmt = connection.createStatement();
+        for(String query : queries) {
+            stmt.addBatch(query);
+        }
+        int[] updated = stmt.executeBatch();
+        connection.commit();
+        stmt.close();
+        connection.close();
+        return updated;
+    }
+
+    public <T> T execQuery(final String query, ResultHandler<T> handler)
+            throws SQLException {
+        Statement stmt = connection.createStatement();
+        stmt.execute(query);
+        ResultSet result = stmt.getResultSet();
+        T value = handler.handle(result);
+        result.close();
+        stmt.close();
+        connection.close();
+        return value;
+    }
+}
