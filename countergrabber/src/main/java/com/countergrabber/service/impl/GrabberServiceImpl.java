97e91ffb2101bd8944c9af726e4b27b7f9f89fea
olyver <demar.07@gmail.com>
2017-02-25 01:50:01 +0900

    Has been modified grabber service.

--- countergrabber/src/main/java/com/countergrabber/service/impl/GrabberServiceImpl.java
+++ countergrabber/src/main/java/com/countergrabber/service/impl/GrabberServiceImpl.java
@@ -1,19 +1,21 @@
 package com.countergrabber.service.impl;
 
-import com.countergrabber.dao.CounterDAO;
 import com.countergrabber.dao.SourceDAO;
-import com.countergrabber.domain.Source;
+import com.countergrabber.domain.*;
 import com.countergrabber.service.GrabberService;
 import com.gargoylesoftware.htmlunit.JavaScriptPage;
 import com.gargoylesoftware.htmlunit.WebClient;
+import sun.plugin.dom.exception.InvalidStateException;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class GrabberServiceImpl implements GrabberService {
 
+    private static final int PARTS_IN_LINE = 2;
     private SourceDAO sourceDAO;
+    private List<Counter> counters = new ArrayList<>();
 
     public GrabberServiceImpl(SourceDAO sourceDAO) {
         this.sourceDAO = sourceDAO;
@@ -23,11 +25,13 @@ public class GrabberServiceImpl implements GrabberService {
     public void grab() {
         List<Source> sources = sourceDAO.findAll();
         for(Source source : sources) {
-            System.out.println(source.getAddress());
             String[] parts = getPage(source.getAddress());
-            System.out.println(parts.length);
-            System.out.println("--------------------------");
+            buildCounter(parts, source.getId());
         }
+    }
+
+    public List<Counter> getCounters() {
+        return counters;
     }
 
     private String[] getPage(final String url) {
@@ -37,18 +41,93 @@ public class GrabberServiceImpl implements GrabberService {
             final JavaScriptPage page = webClient.getPage(url);
             final String pageAsText = page.getContent();
             parts = pageAsText.split("\\r\\n");
-            Arrays.stream(parts).forEach(System.out::println);
         } catch(IOException e) {
             parts = new String[0];
         }
         return parts;
     }
 
-    private void buildCounter(String[] parts) {
+    private void buildCounter(final String[] parts, final long idSource) {
         if(parts.length == 0) {
             return;
         }
-
-
+        if(!parts[0].contains("site")) {
+            return;
+        }
+        Map<String, Integer> valueTypeMap = new HashMap<>();
+        for(int i = 1; i < parts.length; i++) {
+            valueTypeMap = recognizePart(parts[i]);
+            if(valueTypeMap.size() > 0) {
+                Date now = new Date();
+                counters.add(new Counter(
+                        0,
+                        idSource,
+                        now,
+                        valueTypeMap.get("counterType"),
+                        valueTypeMap.get("counterValue"),
+                        now
+                ));
+            }
+        }
     }
+
+    private Map<String, Integer> recognizePart(final String line) {
+        Map<String, Integer> result = new HashMap<>();
+
+        if(line == null) return result;
+
+        Map<String, String> parts;
+        try {
+            parts = splitLine(line);
+        } catch(InvalidStateException | NumberFormatException ignore) {
+            return result;
+        }
+
+        int counterValue = getCounterValue(parts);
+        int counterType = getCounterType(parts);
+
+        result.put("counterValue", counterValue);
+        result.put("counterType", counterType);
+        return result;
+    }
+
+    private Map<String, String> splitLine(final String line) {
+        String[] parts = line.split("=");
+        if(parts.length < PARTS_IN_LINE) {
+            throw new InvalidStateException("Not enough parameters.");
+        }
+        Map<String, String> partMap = new HashMap<>();
+        partMap.put("counterName", parts[0]);
+        partMap.put("counterValue", parts[1]);
+        return partMap;
+    }
+
+    private int getCounterValue(final Map<String, String> parts) {
+        String value = parts.get("counterValue").trim().replace(";", "");
+        return Integer.parseInt(value);
+    }
+
+    private int getCounterType(final Map<String, String> parts) {
+        String counteraName = parts.get("counterName");
+
+        CounterTypes[] hitTypes = HitCounterTypes.values();
+        CounterTypes[] visTypes = VisCounterTypes.values();
+
+        int typesCount = hitTypes.length + visTypes.length;
+        CounterTypes[] counterTypes = new CounterTypes[typesCount];
+
+        System.arraycopy(hitTypes, 0, counterTypes, 0, hitTypes.length);
+        System.arraycopy(visTypes, 0, counterTypes, hitTypes.length, visTypes.length);
+
+        int counterType = 0;
+        for (CounterTypes type : counterTypes) {
+            if(counteraName.contains(type.getName())) {
+                counterType = type.getType();
+                break;
+            }
+        }
+        return counterType;
+    }
+
+
 }
