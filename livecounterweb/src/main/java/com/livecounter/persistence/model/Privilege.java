bbb84fcdfbb948d508eb2bdd9451830aa9d805ae
olyver <demar.07@gmail.com>
2017-03-06 01:33:23 +0900

    Has been added new domains.

---
+++ livecounterweb/src/main/java/com/livecounter/persistence/model/Privilege.java
@@ -0,0 +1,50 @@
+package com.livecounter.persistence.model;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name = "privileges")
+public class Privilege {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "privileges", fetch = FetchType.EAGER)
+    private Collection<Role> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "Privilege{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
