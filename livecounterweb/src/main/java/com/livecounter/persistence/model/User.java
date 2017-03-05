62bcad778bb876ca3e573d9dbde9939100c7f452
olyver <demar.07@gmail.com>
2017-03-05 15:22:43 +0900

    Has been changed GenerationType to IDENTITY. Has been changed annotated column names for firstName and lastName.

--- livecounterweb/src/main/java/com/livecounter/persistence/model/User.java
+++ livecounterweb/src/main/java/com/livecounter/persistence/model/User.java
@@ -6,11 +6,13 @@ import javax.persistence.*;
 @Table(name = "user_account")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 
     private String email;
