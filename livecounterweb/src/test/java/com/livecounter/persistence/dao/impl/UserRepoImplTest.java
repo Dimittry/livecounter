4f27487d86d4fdd8cd1f186242ef6e56a55b3422
olyver <demar.07@gmail.com>
2017-03-05 15:27:45 +0900

    Has been added tests for findEmail and save methods.

--- livecounterweb/src/test/java/com/livecounter/persistence/dao/impl/UserRepoImplTest.java
+++ livecounterweb/src/test/java/com/livecounter/persistence/dao/impl/UserRepoImplTest.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.persistence.NoResultException;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -20,10 +21,32 @@ public class UserRepoImplTest {
     private UserRepo userRepo;
 
     @Test
-    public void findAll() throws Exception {
+    public void testFindAll() throws Exception {
         List<User> userList = userRepo.findAll();
         User user = userList.get(0);
         assertEquals(1, userList.size());
+    }
+
+    @Test
+    public void testFindByEmail() throws Exception {
+        User user = userRepo.findByEmail("test@test.con");
+        assertNotNull(user);
+        assertEquals("test1", user.getFirstName());
+    }
+
+    @Test(expected = NoResultException.class)
+    public void testNotFindByEmail() {
+        User user = userRepo.findByEmail("test@test.com");
+    }
+
+    @Test
+    public void testSave() {
+        User user = new User();
+        user.setFirstName("test2fn");
+        user.setLastName("test2ln");
+        user.setPassword("testpass2");
+        user.setEmail("test2@test2.com");
+        userRepo.save(user);
     }
 
 }
