b8059cd986c661dac1fca873b8725470391a90ba
olyver <demar.07@gmail.com>
2017-03-06 23:38:04 +0900

    Has been modified finByEmail

--- livecounterweb/src/main/java/com/livecounter/persistence/dao/impl/UserRepoImpl.java
+++ livecounterweb/src/main/java/com/livecounter/persistence/dao/impl/UserRepoImpl.java
@@ -2,6 +2,8 @@ package com.livecounter.persistence.dao.impl;
 
 
 import com.livecounter.persistence.dao.UserRepo;
+import com.livecounter.persistence.model.Privilege;
+import com.livecounter.persistence.model.Role;
 import com.livecounter.persistence.model.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -36,11 +38,15 @@ public class UserRepoImpl implements UserRepo {
 
     @Override
     public User findByEmail(final String email) {
-        return (User)session().createQuery("from User u where email = :email").setParameter("email", email).getSingleResult();
+        List<User> users =
+                (List<User>)session().createQuery("from User u where email = :email").setParameter("email", email).list();
+        return (users.size() > 0) ? users.get(0) : null;
     }
 
     @Override
-    public void save(final User user) {
+    public User save(final User user) {
         session().persist(user);
+        return user;
     }
+
 }
