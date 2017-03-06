f4edbebd620cc0f2671b48edd2d59906a2a7f1d1
olyver <demar.07@gmail.com>
2017-03-06 23:38:29 +0900

    Has been modified findByEmail.

--- livecounterweb/src/main/java/com/livecounter/persistence/dao/UserRepo.java
+++ livecounterweb/src/main/java/com/livecounter/persistence/dao/UserRepo.java
@@ -1,12 +1,17 @@
 package com.livecounter.persistence.dao;
 
 
+import com.livecounter.persistence.model.Privilege;
+import com.livecounter.persistence.model.Role;
 import com.livecounter.persistence.model.User;
 
 import java.util.List;
 
 public interface UserRepo {
     List<User> findAll();
+
     User findByEmail(String email);
-    void save(User user);
+
+    User save(User user);
+
 }
