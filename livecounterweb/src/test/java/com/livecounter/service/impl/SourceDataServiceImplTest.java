6e357d58da70865e49e00f89306bedc634005af0
olyver <demar.07@gmail.com>
2017-04-03 23:41:47 +0900

    Has been implemented sourceDataService tests.

--- livecounterweb/src/test/java/com/livecounter/service/impl/SourceDataServiceImplTest.java
+++ livecounterweb/src/test/java/com/livecounter/service/impl/SourceDataServiceImplTest.java
@@ -8,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.Assert.*;
 
@@ -17,15 +20,35 @@ import static org.junit.Assert.*;
 public class SourceDataServiceImplTest {
     @Autowired
     private SourceDataService sourceDataService;
+
     @Test
-    public void testSourceDataSave() {
+    public void testSourceDataSaveFake() {
+        sourceDataService.save(getSourceDataInstance());
+        assertNotNull(sourceDataService);
+    }
+
+    @Test
+    public void testSourceDataBatchSaveFake() {
+        List<SourceData> sourceDatas = new ArrayList<>();
+        sourceDatas.add(getSourceDataInstance());
+        sourceDatas.add(getSourceDataInstance());
+        sourceDatas.add(getSourceDataInstance());
+        sourceDatas.add(getSourceDataInstance());
+        sourceDataService.saveAll(sourceDatas);
+        assertNotNull(sourceDataService);
+    }
+
+    private SourceData getSourceDataInstance() {
+        long value = ThreadLocalRandom.current().nextLong(1,2000000);
+        int type = ThreadLocalRandom.current().nextInt(1, 100);
+        long idSource = ThreadLocalRandom.current().nextLong(1, 1500);
+
         SourceData sourceData = new SourceData();
         sourceData.setDay(new Date());
-        sourceData.setValue(777L);
-        sourceData.setType(7);
-        sourceData.setIdSource(7L);
+        sourceData.setValue(value);
+        sourceData.setType(type);
+        sourceData.setIdSource(idSource);
         sourceData.setCreated(new Date());
-        sourceDataService.save(sourceData);
-        assertNotNull(sourceDataService);
+        return sourceData;
     }
 }
