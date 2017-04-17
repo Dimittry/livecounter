8e2675406ec9a471d8e61f4ba2e1727df8cd6a10
olyver <demar.07@gmail.com>
2017-04-17 23:47:55 +0900

    Modify batch insert.

--- livecounterweb/src/main/java/com/livecounter/persistence/dao/impl/SourceDataRepoImpl.java
+++ livecounterweb/src/main/java/com/livecounter/persistence/dao/impl/SourceDataRepoImpl.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.FlushModeType;
 import java.util.List;
 
 @Repository
 public class SourceDataRepoImpl extends BaseRepoImpl implements SourceDataRepo {
+        private static final int batchSize = 10;
 
     @Override
     public SourceData save(final SourceData sourceData) {
@@ -23,11 +25,12 @@ public class SourceDataRepoImpl extends BaseRepoImpl implements SourceDataRepo {
     @Override
     @Transactional
     public void saveAll(final List<SourceData> sourceDatas) {
-//        Session session = session();
-//        for(int i = 0; i < sourceDatas.size(); i++) {
-//            session.save(sourceDatas.get(i));
-//        }
-//        session.flush();
-//        session.clear();
+        for(int i = 0; i < sourceDatas.size(); i++) {
+            em.persist(sourceDatas.get(i));
+            if(i % batchSize == 0 && i > 0) {
+                em.flush();
+                em.clear();
+            }
+        }
     }
 }
