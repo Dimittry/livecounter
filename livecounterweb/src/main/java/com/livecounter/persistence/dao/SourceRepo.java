2b1f1490eb3ca0fa6976439902487bae6872aed8
olyver <demar.07@gmail.com>
2017-04-19 23:20:43 +0900

    Implement query with named query

--- livecounterweb/src/main/java/com/livecounter/persistence/dao/SourceRepo.java
+++ livecounterweb/src/main/java/com/livecounter/persistence/dao/SourceRepo.java
@@ -2,11 +2,15 @@ package com.livecounter.persistence.dao;
 
 
 import com.livecounter.persistence.model.Source;
+import com.livecounter.persistence.model.SourceData;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface SourceRepo {
     List<Source> findAll();
     Source findSourceByName(String name);
     Source persist(final Source source);
+    List<Source> getByStartEndDate(final LocalDate startDate, final LocalDate endDate);
+
 }
--- livecounterweb/src/main/java/com/livecounter/persistence/dao/impl/SourceRepoImpl.java
+++ livecounterweb/src/main/java/com/livecounter/persistence/dao/impl/SourceRepoImpl.java
@@ -2,11 +2,13 @@ package com.livecounter.persistence.dao.impl;
 
 import com.livecounter.persistence.dao.SourceRepo;
 import com.livecounter.persistence.model.Source;
+import com.livecounter.persistence.model.SourceData;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -34,5 +36,13 @@ public class SourceRepoImpl extends BaseRepoImpl implements SourceRepo {
         } else {
             return em.merge(source);
         }
+    }
+
+    @Override
+    public List<Source> getByStartEndDate(final LocalDate startDate, final LocalDate endDate) {
+        return em.createNamedQuery(Source.FIND_BETWEEN_DATES, Source.class)
+                .setParameter("startDate", startDate)
+                .setParameter("endDate", endDate)
+                .getResultList();
     }
 }
