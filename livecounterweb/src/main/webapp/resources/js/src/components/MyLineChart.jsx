28834b695a2716c2eba19912d5841fa6f7b5e456
olyver <demar.07@gmail.com>
2017-05-03 22:50:08 +0900

    Configure chart to display correct lines

--- livecounterweb/src/main/webapp/resources/js/src/components/MyLineChart.jsx
+++ livecounterweb/src/main/webapp/resources/js/src/components/MyLineChart.jsx
@@ -22,9 +22,29 @@ export default class MyLineChart extends React.Component {
                 let datasets = [];
                 let labels = [];
                 res.data.map(source => {
+                    let first = source.sourceData[0];
+                    console.log(first);
                     let dataset = {
                         'label' : source.checkName,
-                        'data' : source.sourceData.filter(elem => (elem.type == type)).map(elem => elem.value)
+                        'data' : source.sourceData.filter(elem => (elem.type == type)).map(elem => elem.value),
+                        'backgroundColor' : "rgba("+source.color+", 1)",
+                        'borderColor' : "rgba("+source.color+", 1)",
+                        'borderWidth' : 2,
+                        'fill' : false,
+                        'lineTension': 0.1,
+                        'borderCapStyle': 'butt',
+                        'borderDash': [],
+                        'borderDashOffset': 0.0,
+                        'borderJoinStyle': 'miter',
+                        'pointBorderColor': "rgba("+source.color+", 1)",
+                        'pointBackgroundColor': "#fff",
+                        'pointBorderWidth': 1,
+                        'pointHoverRadius': 6,
+                        'pointHoverBackgroundColor': "rgba("+source.color+", 1)",
+                        'pointHoverBorderColor': "rgba(220,220,220,1)",
+                        'pointHoverBorderWidth': 2,
+                        'pointRadius': 3,
+                        'pointHitRadius': 10,
                     };
                     datasets.push(dataset);
                     if(labels.length === 0) {
