3460ae736fb799ede1655cf9d59c1dd2caaa4f9d
olyver <demar.07@gmail.com>
2017-05-08 23:55:20 +0900

    Add building menu from props.

--- livecounterweb/src/main/webapp/resources/js/src/components/Sidebar.jsx
+++ livecounterweb/src/main/webapp/resources/js/src/components/Sidebar.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 export default class Sidebar extends React.Component {
     render() {
+        const menuItems = this.props.menuItems || {};
+        let menuHtml = Object.keys(menuItems).map((key) => {
+            let item = menuItems[key];
+            return <li><a href={item.link}>{item.title}</a></li>
+        });
         return (
             <div className="col-sm-3 col-md-2 sidebar">
                 <ul className="nav nav-sidebar">
-                    <li className="active"><a href="#">Overview <span className="sr-only">(current)</span></a></li>
-                    <li><a href="#">Reports</a></li>
-                    <li><a href="#">Analytics</a></li>
-                    <li><a href="#">Export</a></li>
+                    {/*<li className="active"><a href="#">Overview <span className="sr-only">(current)</span></a></li>*/}
+                    {menuHtml}
                 </ul>
             </div>
         );
