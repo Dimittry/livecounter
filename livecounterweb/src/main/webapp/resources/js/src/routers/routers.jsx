76b835a02ba5065f8bacf43ac368e9c73c2a1986
olyver <demar.07@gmail.com>
2017-05-08 23:55:44 +0900

    Add router

---
+++ livecounterweb/src/main/webapp/resources/js/src/routers/routers.jsx
@@ -0,0 +1,10 @@
+import {Route} from 'react-router-dom';
+
+import App from "../components/App";
+import HomePage from "../components/HomePage";
+
+export default (
+    <Route path="/" component={App}>
+
+    </Route>
+);
