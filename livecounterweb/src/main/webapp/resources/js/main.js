b90743a938119b40ba2d3e25b44e299180dd7ca1
olyver <demar.07@gmail.com>
2017-05-08 23:56:07 +0900

    Change to router

--- livecounterweb/src/main/webapp/resources/js/main.js
+++ livecounterweb/src/main/webapp/resources/js/main.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import App from './src/components/App.jsx';
+// import routes from "./src/routers/routers";
 
 require('../css/style.css');
 require("bootstrap/dist/css/bootstrap.css")
 require("jquery")
 require("bootstrap/dist/js/bootstrap.min.js")
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Route path="/" component={App}/>
+    </Router>
+    , document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
