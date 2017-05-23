ba4ccf1c5d77430f0b6d628e9bd6cc5cf01da2a1
olyver <demar.07@gmail.com>
2017-05-23 22:54:02 +0900

    Add profile files

---
+++ livecounterweb/src/main/webapp/resources/js/src/components/Profile.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProfilePage from '../pages/ProfilePage.jsx';
+
+export default class Profile extends React.Component {
+    render() {
+        return (<div>
+            <ProfilePage />
+        </div>);
+    }
+}
---
+++ livecounterweb/src/main/webapp/resources/js/src/pages/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ProfilePage extends React.Component {
+    render() {
+        return (
+            <div className="col-sm-12 col-md-12 main">
+                <h1 className="page-header">Profile</h1>
+
+            </div>
+        );
+    }
+}
