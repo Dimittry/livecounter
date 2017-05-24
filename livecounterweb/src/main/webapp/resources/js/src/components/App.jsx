23f578d85237d073ec2fdf21d74e498943bf57e6
olyver <demar.07@gmail.com>
2017-05-24 23:56:58 +0900

    Change routing

--- livecounterweb/src/main/webapp/resources/js/src/components/App.jsx
+++ livecounterweb/src/main/webapp/resources/js/src/components/App.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
-import {Switch} from 'react-router';
-import {Route} from 'react-router-dom';
 
 import { Button } from 'react-bootstrap';
-import Navigation from './Navigation.jsx';
 import HomePage from './HomePage.jsx';
 import Profile from './Profile.jsx';
+import Login from './Login.jsx';
+
+var Layout = require('./Layout.jsx');
+var LayoutWithoutHeader = require('./LayoutWithoutHeader.jsx');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Switch = ReactRouter.Switch;
 
 export default class App extends React.Component {
 
     render() {
         return (
-            <div>
-                <Navigation />
-                <div className="container-fluid">
-                    <div className="row">
-                        <Switch>
-                            <Route exact path="/" component={HomePage}/>
-                            <Route exact path="/profile" component={Profile}/>
-                        </Switch>
-                    </div>
-                </div>
-            </div>
-
+            <Router>
+                <Switch>
+                    <Layout exact path="/" component={HomePage}/>
+                    <Layout path="/profile" component={Profile}/>
+                    <LayoutWithoutHeader path="/login" component={Login}/>
+                </Switch>
+            </Router>
         );
     }
 }
